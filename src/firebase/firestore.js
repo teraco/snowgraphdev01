7e6e68c7a9f2693f071a03389db23abf65c07e27
unknown <teraco@gmail.com>
2019-10-13 17:34:32 +0900

    不具合修正

--- src/firebase/firestore.js
+++ src/firebase/firestore.js
@@ -1,13 +1,6 @@
 import firebase from 'firebase';
 
 const config = {
-  apiKey: "AIzaSyBihzU2qwLYCuBU3rqiSpG36SajKDxzp8M",
-  authDomain: "snowgraphdev01.firebaseapp.com",
-  databaseURL: "https://snowgraphdev01.firebaseio.com",
-  projectId: "snowgraphdev01",
-  storageBucket: "snowgraphdev01.appspot.com",
-  messagingSenderId: "928252924151",
-  appId: "1:928252924151:web:866ca12f9ebd8b9efcc3f2",
 };
 
 const settings = { timestampsInSnapshots: true };
